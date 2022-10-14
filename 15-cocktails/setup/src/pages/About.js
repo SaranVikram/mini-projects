5845c422561e1cdb4ff6d2aa4c8f2dec4987ceba
vikram <saran3vikram@gmail.com>
2022-10-14 15:46:37 +0530

    cocktails using fetch

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from "react"
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h2 className="section-title">about us</h2>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et accusamus hic natus voluptatem nulla esse delectus praesentium id modi? Saepe, officiis. Ut culpa assumenda, illo nesciunt repellendus atque tempore eum.</p>
+    </section>
   )
 }
 
--- 15-cocktails/setup/src/pages/SingleCocktail.js
+++ 15-cocktails/setup/src/pages/SingleCocktail.js
@@ -1,13 +1,51 @@
-import React from 'react'
-import Loading from '../components/Loading'
-import { useParams, Link } from 'react-router-dom'
-const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
+import React from "react"
+import Loading from "../components/Loading"
+import { useParams, Link } from "react-router-dom"
+import { useEffect } from "react"
+import { useState } from "react"
+const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i="
 
 const SingleCocktail = () => {
+  const { id } = useParams()
+  const [loading, setLoading] = useState(true)
+  const [drink, setDrink] = useState({})
+  const fetchData = async () => {
+    try {
+      const resp = await fetch(`${url}${id}`)
+      const data = await resp.json()
+      const { drinks } = data
+      const singleDrink = {
+        id: drinks[0].idDrink,
+        name: drinks[0].strDrink,
+        image: drinks[0].strDrinkThumb,
+        glass: drinks[0].strGlass,
+        preparation: drinks[0].strInstructions,
+      }
+      setDrink(singleDrink)
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  }
+  useEffect(() => {
+    fetchData()
+  }, [])
+  if (loading) {
+    return <Loading />
+  }
+
   return (
-    <div>
-      <h2>single cocktail page </h2>
-    </div>
+    <article className="cocktail">
+      <div className="img-container">
+        <img src={drink.image} alt={drink.name} />
+      </div>
+      <div className="cocktail-footer">
+        <h3>{drink.name}</h3>
+        <h4>{drink.glass}</h4>
+        <p>{drink.preparation}</p>
+      </div>
+    </article>
   )
 }
 
